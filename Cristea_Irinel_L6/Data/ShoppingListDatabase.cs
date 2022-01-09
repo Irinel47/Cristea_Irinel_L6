42e9839942bd75cfd88fc710704fac46d9d0e9ea
Irinel47 <cristea.irinel47@gmail.com>
2022-01-09 18:50:20 +0200

    Add project files.

---
+++ Cristea_Irinel_L6/Data/ShoppingListDatabase.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Cristea_Irinel_L6.Models;
+
+namespace Cristea_Irinel_L6.Data
+{
+    public class ShoppingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Models.ShopList>().Wait();
+        }
+        public Task<List<Models.ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<Models.ShopList>().ToListAsync();
+        }
+        public Task<Models.ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<Models.ShopList>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(Models.ShopList slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(Models.ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+    }
+}
---
+++ Cristea_Irinel_L6/ListEntryPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Cristea_Irinel_L6.Data;
+using Cristea_Irinel_L6.Models;
+
+namespace Cristea_Irinel_L6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class ListEntryPage : ContentPage
+    {
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetShopListsAsync();
+        }
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = new Models.ShopList()
+            });
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ListPage
+                {
+                    BindingContext = e.SelectedItem as Models.ShopList
+                });
+            }
+        }
+
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+        
+    }
+
+    class listView
+    {
+        public static List<ShopList> ItemsSource { get; internal set; }
+    }
+}
